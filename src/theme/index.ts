c0882b4966f4140834401f496fa180efa5b7d377
devin-efendy <devinefendy.dev@gmail.com>
2023-01-28 16:06:11 -0600

    create theme

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+const theme = {
+  text: {
+    primary: "white",
+  },
+  bg: {
+    base: "#27272A",
+  },
+  border: {
+    base: "#52525B",
+    divider: "#71717a",
+  },
+  "icon-button": {
+    base: "#18181B",
+    hover: "#3F3F46",
+    active: "#3B82F6",
+    color: "#A1A1AA"
+  },
+  outline: {
+    active: "#3B82F6",
+  },
+};
+
+export default theme;
